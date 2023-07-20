8d0512e1a56c9db4b4f3fad7e04e4ed95c78eb1c
nnjokunneka111@gmail.com <nnjokunneka111@gmail.com>
2023-07-20 01:50:03 +0100

    save

---
+++ src/components/TodoForm/index.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+function TodoForm() {
+    const [input, setInput] = useState();
+  return (
+    <form className='todo-form'>
+
+    </form>
+  )
+}
+
+export default TodoForm
