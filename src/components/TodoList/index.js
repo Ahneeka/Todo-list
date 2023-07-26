1bed1c3927079ad461358481601d97d56e49750e
nnjokunneka111@gmail.com <nnjokunneka111@gmail.com>
2023-07-26 02:51:43 +0100

    save

--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -1,14 +1,27 @@
 import { Icon } from '@iconify/react';
 
-function TodoList() {
+function TodoList(props) {
+  const handledelete = () => {
+    
+  };
+
   return (
-    <div className="m-8 w-[20rem] ml-auto mr-auto ">
-        <h2 className="bg-blue-500 p-6 text-white mb-3 text-xl flex gap-10">To learn dart  <Icon icon="typcn:delete-outline" width={30} height={30}/> <Icon icon="ep:edit" width={30} height={30} /> </h2>
-        <h2 className="bg- bg-pink-200 p-6 text-white mb-3 text-xl flex gap-10">Work out <Icon icon="typcn:delete-outline" width={30} height={30}/> <Icon icon="ep:edit" width={30} height={30} /> </h2>
-        <h2 className="bg-rose-500 p-6 text-white mb-3 text-xl flex gap-10">Wash cloth <Icon icon="typcn:delete-outline" width={30} height={30}/> <Icon icon="ep:edit" width={30} height={30} /> </h2>
-        <h2 className="bg-violet-500 p-6 text-white mb-3 text-xl flex gap-10"> To Cook <Icon icon="typcn:delete-outline" width={30} height={30}/> <Icon icon="ep:edit" width={30} height={30} /> </h2>
+    <div className= {`m-8 w-[20rem] ml-auto mr-auto p-6 text-white mb-3 text-xl flex gap-9 ${props.className}`}>
+      <h1>
+        {props.title}
+      </h1>
+
+      <button>
+        <Icon icon="typcn:delete-outline" width={30} height={30}/> 
+      </button>
+
+      <button>
+        <Icon icon="ep:edit" width={30} height={30} />
+      </button>
     </div>
   )
 }
 
 export default TodoList
+
+
